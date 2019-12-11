cd01d689abd3f783439f1aaf5c0ff194b1d6afaf
yellowglasses20 <yano.makoto20@gmail.com>
2019-12-11 16:25:21 +0900

    初期登録

---
+++ scrapbox.go
@@ -0,0 +1,109 @@
+package scrapbox
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"reflect"
+
+	"github.com/google/go-querystring/query"
+)
+
+const (
+	defaultBaseURL = "https://scrapbox.io/api/pages/"
+	defaultSkip    = 0
+	defaultLimit   = 100
+	userAgent      = "go-scrapbox"
+)
+
+// Client is scrapbox API client
+type Client struct {
+	client      *http.Client
+	BaseURL     *url.URL
+	ProjectName string
+	UserAgent   string
+}
+
+// Create http query string from map
+func addOptions(s string, opt interface{}) (string, error) {
+	v := reflect.ValueOf(opt)
+	if v.Kind() == reflect.Ptr && v.IsNil() {
+		return s, nil
+	}
+
+	u, err := url.Parse(s)
+	if err != nil {
+		return s, err
+	}
+
+	qs, err := query.Values(opt)
+	if err != nil {
+		return s, err
+	}
+
+	u.RawQuery = qs.Encode()
+	return u.String(), nil
+}
+
+// NewClient returns a new scrapbox API client.
+func NewClient(projectName string) (*Client, error) {
+	if projectName == "" {
+		return nil, fmt.Errorf("projectName is %q, but this is required parameter", projectName)
+	}
+	baseURL, _ := url.Parse(defaultBaseURL)
+
+	c := &Client{client: &http.Client{}, BaseURL: baseURL, ProjectName: projectName, UserAgent: userAgent}
+	return c, nil
+}
+
+// NewRequest returns new http request
+func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Request, error) {
+	rel, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	u := c.BaseURL.ResolveReference(rel)
+
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Accept", "application/json")
+	if c.UserAgent != "" {
+		req.Header.Add("User-Agent", c.UserAgent)
+	}
+	return req, nil
+}
+
+// Do http request
+func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if v != nil {
+		if w, ok := v.(io.Writer); ok {
+			io.Copy(w, resp.Body)
+		} else {
+			err = json.NewDecoder(resp.Body).Decode(v)
+		}
+	}
+	return resp, err
+}
